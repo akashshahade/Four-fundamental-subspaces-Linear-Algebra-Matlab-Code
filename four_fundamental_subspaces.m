674561414a5330c0dd71f0edbcd55092a7338ee6
Akash Shahade <31156238+akashshahade@users.noreply.github.com>
2021-01-09 08:18:33 +0530

    Update four_fundamental_subspaces.m

--- four_fundamental_subspaces.m
+++ four_fundamental_subspaces.m
@@ -1,13 +1,17 @@
 clc
 clear 
 
+% Write any matrix in this form
+
 A=[0 2 3 4;0 6 7 8;0 10 11 12].';
+
 % Bases of four fundamental vector spaces associated
 % with the matrix A.
 % cs- basis of the column space of A
 % ns- basis of the nullspace of A
 % rs- basis of the row space of A
 % lns- basis of the left nullspace of A
+
 [V, pivot] = rref(A);
 r = length(pivot);
 cs = A(:,pivot);
